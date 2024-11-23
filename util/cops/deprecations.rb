7b1071607c3cc4445aa352d4969e3711f455e47c
BobbyDigitz <76012943+BobbyDigitz@users.noreply.github.com>
2024-11-22 16:19:28 -0800

    Update deprecations.rb

--- util/cops/deprecations.rb
+++ util/cops/deprecations.rb
@@ -1,20 +1,4 @@
-# frozen_string_literal: true
-
-module RuboCop
-  module Cop
-    module Rubygems
-      # This cop enforces that no outdated deprecations are present on RubyGems
-      # code base.
-      #
-      # @example
-      #
-      #   # bad
-      #   rubygems_deprecate :safdfa, :none
-      #
-      #   # good
-      #   # the `deprecate` call is fully removed
-      #
-      class Deprecations < Cop
+s < Cop
         def on_send(node)
           _receiver, method_name, *args = *node
           return unless method_name == :rubygems_deprecate || method_name == :rubygems_deprecate_command
