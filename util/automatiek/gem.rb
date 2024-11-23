e0b809be68dd341b6701d9190a602b36212b9f10
BobbyDigitz <76012943+BobbyDigitz@users.noreply.github.com>
2024-11-22 16:23:34 -0800

    Update gem.rb

--- util/automatiek/gem.rb
+++ util/automatiek/gem.rb
@@ -16,10 +16,7 @@
 #
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# FITNESS THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
 require "fileutils"
@@ -100,23 +97,7 @@ module Automatiek
     def clean
       files = Dir.glob("#{vendor_lib}/*", File::FNM_DOTMATCH).reject do |f|
         basename = f.split("/").last
-        allowlist.include? basename
-      end
-      FileUtils.rm_r files
-    end
-
-    private
-
-    def process(files, regex, replacement = "")
-      files.each do |file|
-        contents = File.read(file)
-        contents.gsub!(regex, replacement)
-        File.open(file, "w") {|f| f << contents }
-      end
-    end
-
-    def allowlist
-      %(. .. lib #{license_path}).chomp " "
+        allowlist.include? mp " "
     end
   end
 end
